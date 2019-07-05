f959c42344f1c143d83ff8b4ccb9a667928a3a6b
kmi010 <kmi010@uit.no>
2019-07-05 13:59:55 +0200

    Update example.m
    Randomly removed 50% of the values in the synthetic data to illustrate how TCK works with missing data.

--- example.m
+++ example.m
@@ -9,6 +9,16 @@ load('xte_VAR.mat');
 Xte = reshape(xte,[200,50,2]);
 Yte = Y;
 
+% add missing data to the synthetic data
+Z = ones(size(X));
+R = rand(size(X));
+Z(R>0.5) = 0;
+X(Z==0) = nan;
+Zte = ones(size(Xte));
+Rte = rand(size(Xte));
+Zte(Rte>0.5) = 0;
+Xte(Zte==0) = nan;
+
 
 % Train GMM models
 [GMMpar,C,G]  = trainTCK(X);
